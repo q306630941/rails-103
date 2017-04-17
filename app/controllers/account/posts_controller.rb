d56456596d73df9f6798aae5055096f795a58680
q306630941 <kiwileaf@icloud.com>
2017-04-17 20:09:46 +0800

    implement my groups

---
+++ app/controllers/account/posts_controller.rb
@@ -0,0 +1,12 @@
+class Account::PostsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @posts = current_user.posts
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path, alert: "Post deleted"
+  end
+end
